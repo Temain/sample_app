4b7af0a24b14fe5fb375b1bafa887aa4f5e3946e
Aseev Artyom <asv.artm@gmail.com>
2013-12-28 05:55:48 +0400

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime
+#  updated_at      :datetime
+#  password_digest :string(255)
+#
+
 class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   has_secure_password
@@ -5,7 +17,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
-  before_save { |user| user.email = email.downcase }
+  before_save { email.downcase! }
 
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Aseev Artyom"
+    email    "asv.artm@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
